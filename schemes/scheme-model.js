b50c292e60c72bc19acc14ba129668d3cea23e9f
richknicks <rlynch@gmail.com>
2020-03-19 11:06:57 -0400

    findSteps by id

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -19,10 +19,15 @@ function findById(id) {
     .first()
 }
 
+
 function findSteps(id) {
     return db("steps")
-    .where({id})
-    }
+      .select('st.id', 's.scheme_name', 'st.step_number', 'st.instructions')
+        .from('schemes as s')
+        .join ('steps as st', 'st.scheme_id', '=', 's.id')
+        .where({ scheme_id: id })
+        .orderBy('st.step_number');
+}
 
 function add(scheme) {
     return db("schemes")
